b05c2231bcec1a7e1268c2d07614e01ed6b33597
AngusJull <ajull1@ocdsb.ca>
2019-11-03 22:05:58 -0500

    I set up a project with a couple sprites to make the ISU start a little more easily.
    No real work has been done yet

---
+++ Boids Test/Assets/basicBoids.cs
@@ -0,0 +1,22 @@
+﻿//Unity script by Angus Jull
+
+using UnityEngine;
+
+public class basicBoids : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+    
+    #region Unity Methods
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    #endregion
+}
---
+++ Boids Test/Assets/setSize.cs
@@ -0,0 +1,22 @@
+﻿//Unity script by Angus Jull
+
+using UnityEngine;
+
+public class setSize : MonoBehaviour
+{
+    #region Variables
+    public Camera main;
+    #endregion
+    
+    #region Unity Methods
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    #endregion
+}
