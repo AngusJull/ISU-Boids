3a9ea00339e7b2121a0ed765ff026cf1c5510b54
AngusJull <54853169+AngusJull@users.noreply.github.com>
2019-11-06 11:30:11 -0500

    Made some progress on the boids

---
+++ Boids - Angus Jull/Assets/Scripts/createBoids.cs
@@ -0,0 +1,41 @@
+﻿//Created by Angus Jull on November 5th 2019, creates a certain number of boids when the program starts,
+//and keeps the number of boids there are and the amount the user wants the same at all times
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createBoids : MonoBehaviour
+{
+    #region Variables
+    public GameObject parentBoid;
+    public int targetNumBoids;
+    public int numBoids;
+    #endregion
+
+    #region Unity Functions
+    void Start()
+    {
+        for (numBoids = 0; numBoids < targetNumBoids; ++numBoids)
+        {
+            Instantiate(parentBoid).GetComponent<Transform>().position.Set(numBoids, numBoids, 0);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (numBoids < targetNumBoids)
+        {
+            Instantiate(parentBoid);
+            numBoids++;
+            Debug.Log("Created a Boid");
+        }
+        else if (numBoids > targetNumBoids)
+        {
+            GameObject[] Boids = GameObject.FindGameObjectsWithTag("Boid");
+            Destroy(Boids[0], 0.1f);
+            numBoids--;
+            Debug.Log("Destroyed a boid");
+        }
+    }
+    #endregion
+}
---
+++ Boids - Angus Jull/Assets/Scripts/screenWrap.cs
@@ -0,0 +1,43 @@
+﻿//Created by Angus Jull on November 5th 2019, makes the boid wrap around the screen whenever it's transform 
+//Is no longer visible to the camera
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenWrap : MonoBehaviour
+{
+    #region Variables
+    private CameraBounds bounds;
+    #endregion
+    void Start()
+    {
+        bounds = Camera.main.GetComponent<CameraBounds>();
+    }
+    #region Unity Functions
+    // Update is called once per frame
+    void Update()
+    {
+        //If the player is no longer visible and they have moved over the screen boundaries, their x or y will be set to the opposite values
+        if (!GetComponent<SpriteRenderer>().isVisible)
+        {
+            if (transform.position.x < bounds.minimums.x)
+            {
+                transform.position.Set(bounds.maximums.x, transform.position.y, transform.position.z);
+            }
+            else if (transform.position.x > bounds.maximums.x)
+            {
+                transform.position.Set(bounds.minimums.x, transform.position.y, transform.position.z);
+            }
+            if (transform.position.y < bounds.minimums.y)
+            {
+                transform.position.Set(transform.position.x, bounds.maximums.y, transform.position.z);
+            }
+            else if (transform.position.y > bounds.maximums.y)
+            {
+                transform.position.Set(transform.position.x, bounds.minimums.y, transform.position.z);
+            }
+        }
+        #endregion
+    }
+}
+     
