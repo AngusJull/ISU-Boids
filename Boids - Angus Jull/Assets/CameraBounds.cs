6b9926da29b8baf5d8e5d00ad4c32353b80ee976
AngusJull <ajull1@ocdsb.ca>
2019-11-05 22:49:04 -0500

    Created a new project
    This is the project that the boids simulation wil be made in.

---
+++ Boids - Angus Jull/Assets/CameraBounds.cs
@@ -0,0 +1,21 @@
+﻿//Unity script by Angus Jull
+
+using UnityEngine;
+
+public class CameraBounds : MonoBehaviour
+{
+    #region Variables
+    #endregion
+    
+    #region Unity Methods
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    #endregion
+}
