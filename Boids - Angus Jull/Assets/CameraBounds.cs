6d1fca097837bbe50e17ff37bfd2caf0f2a467ad
AngusJull <ajull1@ocdsb.ca>
2019-11-05 23:13:16 -0500

    Added the CameraBounds component again
    I made this in class but forgot to push it, I quickly made it again, better this time.

--- Boids - Angus Jull/Assets/CameraBounds.cs
+++ Boids - Angus Jull/Assets/CameraBounds.cs
@@ -1,21 +1,24 @@
-﻿//Unity script by Angus Jull
+﻿//Unity script by Angus Jull, Created on November 5th 2019
+//Sets the screen boundaries that the boids will stay within in two Vector2 objects. 
 
 using UnityEngine;
 
 public class CameraBounds : MonoBehaviour
 {
     #region Variables
+    //These variables are meant to be used with boids.
+    public Vector2 minimums = new Vector2();
+    public Vector2 maximums = new Vector2();
     #endregion
     
     #region Unity Methods
+    //Sets the two Vector2s
     void Start()
     {
-        
-    }
-
-    void Update()
-    {
-        
+        float vertExtent = GetComponent<Camera>().orthographicSize;
+        float horzExtent = vertExtent * Screen.width / Screen.height;
+        minimums.Set(-1 * horzExtent, -1 * vertExtent);
+        maximums.Set(horzExtent, vertExtent);
     }
     #endregion
 }
